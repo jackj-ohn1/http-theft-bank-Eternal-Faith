33d5167ff5b87419d0bf35b9ff9fbd6f9d7c685b
jackj-ohn1 <92258893+jackj-ohn1@users.noreply.github.com>
2021-11-22 17:15:38 +0800

    Update main.go

--- checkpoint2/main.go
+++ checkpoint2/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/Grand-Theft-Auto-In-CCNU-MUXI/hacker-support/httptool"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	req, err := httptool.NewRequest(
+	/*req, err := httptool.NewRequest(
 		"",
 		"",
 		"",
@@ -19,5 +20,23 @@ func main() {
 	fmt.Println(req)
 
 	// write your code below
-	// ...
+	// ...*/
+	client := &http.Client{}
+	url := "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code"
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("code", "yyj")
+
+	req.Header.Add("passport", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoieXlqIiwiaWF0IjoxNjM3MjA0MDMzLCJuYmYiOjE2MzcyMDQwMzN9.CTGHa5Pq9yAICFsVTC1Xq9dAl3k221j2ERZ2crGlZQ4")
+	if err != nil {
+		fmt.Println(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	content, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(content), err)
+	defer resp.Body.Close()
 }
